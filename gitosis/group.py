miracle2k/gitosis

commit bdf70f5dc75e6988d3241f2ec7db66acae07da99

Simplify group membership generation
The code to recursively list group membership was a bit
overcomplicated. Simplify it, and make the associated test
a bit more challenging, by ensuring that duplicates don't
get added.

gitosis/group.py
@@ -72,12 +72,10 @@ def listMembers(config, group, mset):
             members = members.split()
 
         for user in members:
-            if user in mset:
-                pass
+            if user.startswith('@'):
+                listMembers(config, user[1:], mset)
             else:
                 mset.add(user)
-                if user.startswith('@'):
-                    listMembers(config,user[1:],mset)
 
 
 def generate_group_list_fp(config, fp):
@@ -98,10 +96,9 @@ def generate_group_list_fp(config, fp):
         if group == 'all':
             continue
 
-        items = set()
-        listMembers(config,group,items)
+        users = set()
+        listMembers(config, group, users)
 
-        users = filter(lambda u: not u.startswith('@'), items)
         line = group + ': ' + ' '.join(sorted(users))
         print >>fp, line
 

gitosis/test/test_group.py
@@ -144,7 +144,7 @@ def test_groupList_multiple():
     cfg.add_section('group all')
     cfg.set('group all', 'readonly', 'zap')
     cfg.add_section('group foo')
-    cfg.set('group foo', 'members', 'a b @bar')
+    cfg.set('group foo', 'members', 'a b c @bar')
     cfg.add_section('group bar')
     cfg.set('group bar', 'members', 'c d')
     cfg.add_section('group baz')

commit fc81487a3c5293156536170f7e4964a4776c4090

Fix some breakage from bdf70f5, simplify further
group.listMembers() should include both users and groups in its output
set. Callers that only want users should filter this.

gitosis/group.py
@@ -72,10 +72,9 @@ def listMembers(config, group, mset):
             members = members.split()
 
         for user in members:
+            mset.add(user)
             if user.startswith('@'):
                 listMembers(config, user[1:], mset)
-            else:
-                mset.add(user)
 
 
 def generate_group_list_fp(config, fp):
@@ -96,9 +95,10 @@ def generate_group_list_fp(config, fp):
         if group == 'all':
             continue
 
-        users = set()
-        listMembers(config, group, users)
+        items = set()
+        listMembers(config, group, items)
 
+        users = filter(lambda u: not u.startswith('@'), items)
         line = group + ': ' + ' '.join(sorted(users))
         print >>fp, line
 